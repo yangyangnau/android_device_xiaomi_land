0cb284e40496e906aa55860ad7cc0f178faee01b
Yangyang Nau <yangyangnau@qq.com>
2016-11-12 10:28:00 +0800

    fix TARGET_PLATFORM_DEVICE_BASE.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,7 +152,7 @@ TARGET_NO_RPC := true
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_land
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 TARGET_RECOVERY_DEVICE_MODULES := libinit_land
 
 # Keymaster
